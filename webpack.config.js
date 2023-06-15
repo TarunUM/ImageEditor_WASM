73b18cd926b2da1a33be27a475b56dee3acf98f8
tarun <tarun.8792@gmail.com>
2023-06-15 17:56:46 +0530

    Compiling Rust with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugins = require('html-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
     entry: './public/main.js',
@@ -10,6 +11,9 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugins({
             template: "./public/index.html",
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, '.')
         })
     ]
 }
