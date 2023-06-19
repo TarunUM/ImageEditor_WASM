bc9830c62e0ff7d35809729c8e5154fadc420712
tarun <tarun.8792@gmail.com>
2023-06-19 20:06:41 +0530

    production build completed

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,15 @@ use image::{load_from_memory, ImageOutputFormat::Png};
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) -> String{
-    log(&"1 Grayscale called".into());
-
     let base64_to_vector = decode(encoded_file).unwrap();
-    log(&"2 Image base64 decoded".into());
 
     let mut img = load_from_memory(&base64_to_vector).unwrap();
-    log(&"3 Image decoded".into());
 
     img = img.fliph();
     img = img.grayscale();
-    log(&"4 Grayscale effect applied".into());
 
     let mut buffer = Vec::new();
     img.write_to(&mut buffer, Png).unwrap();
-    log(&"5 New Image writtern".into());
 
     let encoded_image = encode(&buffer);
     let data_url = format!("data:image/png;base64,{}", encoded_image);
